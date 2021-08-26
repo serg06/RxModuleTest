0cc5b39ed40dcedbcd3ae8942a784a25682ef7a8
Serguei <serkhas@hotmail.com>
2021-08-26 19:27:42 -0400

    It works! Here's proof.

--- RxModuleTest/RxModuleTest.cpp
+++ RxModuleTest/RxModuleTest.cpp
@@ -4,6 +4,12 @@ import "RxCpp/Rx/v2/src/rxcpp/rx.hpp";
 
 rxcpp::observable<int> test;
 
+namespace Rx {
+    using namespace rxcpp;
+}
+
+Rx::observable<int> tmp;
+
 int main()
 {
     std::cout << "Hello World!\n";
