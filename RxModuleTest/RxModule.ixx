15c22c8a0c1665cf5fdddeb2597aaa6a80fc2bf6
Serguei <serkhas@hotmail.com>
2021-08-26 19:10:31 -0400

    First commit

---
+++ RxModuleTest/RxModule.ixx
@@ -0,0 +1,10 @@
+module;
+
+#pragma once
+#include "RxCpp/Rx/v2/src/rxcpp/rx.hpp"
+
+export module RxModule;
+
+export namespace rxcpp2 {
+   using namespace rxcpp;
+}
---
+++ RxModuleTest/RxModuleTest.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+import "RxCpp/Rx/v2/src/rxcpp/rx.hpp";
+//import RxModule;
+
+rxcpp::observable<int> test;
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
